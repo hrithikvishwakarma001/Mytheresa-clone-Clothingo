352ecfdf0e7a1b47cad68bf74f24943a95c7e284
Hrithik Vishwakarma <hritik8008@gmail.com>
2022-12-14 23:09:31 +0530

    day2_fw21_0568

---
+++ none.js
@@ -0,0 +1,16 @@
+let hiddenDiv = document.querySelectorAll('.popup');
+let title = document.querySelectorAll('#title ul li');
+console.log(title)
+for (let i = 0; i < hiddenDiv.length; i++) {
+    hiddenDiv[i].style.display = 'none';
+    
+}
+for(let i=0;i<title.length;i++){
+    title[i].onmouseover = ()=>{
+      title[i].style.cursor = 'pointer';
+        hiddenDiv[i].style.display = 'block';
+    }
+    title[i].onmouseout =()=>{
+        hiddenDiv[i].style.display = 'none';
+    }
+}
