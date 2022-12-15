4e9145c59547900bbf61a69464e1e0f76709c2b5
Md Waseem Akram <mdwaseem0603@gmail.com>
2022-12-15 23:01:45 +0530

    day3

---
+++ checkout.js
@@ -0,0 +1,21 @@
+let lsdata=JSON.parse(localStorage.getItem("delivryadd"))||[]
+let proceedbtn=document.querySelector("#proceedbtn")
+let firstname=document.getElementById("firstn").value;
+let lastname=document.getElementById("lastn").value;
+let address=document.getElementById("add").value;
+let pincide=document.getElementById("pincode").value;
+let city=document.getElementById("city").value;
+let number=document.getElementById("number").value
+proceedbtn.addEventListener("click",()=>{
+    window.location.href="card.html"
+    let obj={
+        firstname,
+        lastname,
+        address,
+        pincide,
+        city,
+        number
+    }
+    lsdata.push(obj)
+    localStorage.setItem("delivryadd",JSON.stringify(lsdata))
+})
