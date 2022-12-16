8ae53c0c053a8bbc6007c5b6351f298332b74b67
Hrithik Vishwakarma <hritik8008@gmail.com>
2022-12-16 15:52:28 +0530

    home completed

--- main.js
+++ main.js
@@ -10,3 +10,43 @@ setInterval(()=>{
         saleInterval.innerHTML = a;
     }
 }, 5000);
+
+
+let stickyNav = document.querySelector('.stick');
+let shoppingBagIcon = document.querySelector('#shoppingBagIcon');
+let saleTime = document.querySelector('.saleTime');
+let label = document.querySelector('.label');
+
+let stickyNavTop = stickyNav.offsetTop;
+function stickyNavFunc() {
+    if (window.pageYOffset >= stickyNavTop) {
+        label.style.display = 'none';
+        stickyNav.classList.add('sticky');
+        saleTime.classList.add('sticky3');
+        shoppingBagIcon.classList.add('sticky2');
+
+    } else {
+        label.style.display = 'inline-block';
+        shoppingBagIcon.classList.remove('sticky2');
+        stickyNav.classList.remove('sticky');
+        saleTime.classList.remove('sticky3');
+    }
+}
+window.addEventListener('scroll', stickyNavFunc);
+
+let popup = document.querySelector('.popup');
+ 
+if(window.scrollY > 1){
+    popup.style.display = 'block';
+} else {
+    popup.style.display = 'none';
+}
+let heart2 = document.querySelector('.heart2');
+let wish2 = document.querySelector('.wish-2');
+
+wish2.onmouseover = () => {
+    heart2.style.color = 'rgba(0, 0, 0, 0.6)';
+}
+wish2.onmouseout = () => {
+    heart2.style.color = '#e7e7e7';
+}
